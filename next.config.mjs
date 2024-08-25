0e63a3a191f4ac68891fcbbd63b84e6e01ea6ccb
Diogo Manuel Brito Pires <30441515+El-Banderas@users.noreply.github.com>
2024-08-25 16:58:55 +0100

    Page contacts

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/instagram',
+        destination: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+        permanent: false,
+        basePath: false
+      },
+{
+        source: '/linkedin',
+        destination: 'https://www.youtube.com/watch?v=xvFZjo5PgG0',
+        permanent: false,
+        basePath: false
+      },
+    ]
+  }
+};
 export default nextConfig;
--- src/app/mainPage/Contacts.tsx
+++ src/app/mainPage/Contacts.tsx
@@ -1,11 +1,45 @@
+'use client'
 
+import Image from "next/image";
 import styles from "./Contacts.styles.module.css";
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import InstagramIcon from '@mui/icons-material/Instagram';
+
+import { useRouter } from 'next/navigation'
 
 export default function Contacts() {
-    return  (<div className={styles.contacts}>
-    <div className={styles.content}>
-        <h1>Do you have any questions?<br/>Contact us</h1>
-        <div>Phone</div>
+
+  const router = useRouter()
+    return (<div className={styles.contacts}>
+        <div className={styles.content}>
+            <h1>Do you have any questions?<br />Contact us</h1>
+            <div className={styles.flexHorizontal}>
+                <Image
+                    src="/Contact.png"
+                    alt="Contact us"
+                    width={600}
+                    height={400}
+                />
+                <div className={styles.contactsColumn}>
+                    <div>
+                        Phone<br />999 999 999
+                    </div>
+                    <div>
+                        Mail<br />hello@reallygreatsite.com
+                    </div>
+                    <div></div>
+                    <div>Social</div>
+
+                    <div className={styles.flexHorizontal}>
+                        <div onClick={() => router.push("/instagram")} style={{cursor: 'pointer'}}>
+                            <LinkedInIcon />
+                        </div>
+                        <div onClick={() => router.push("/linkedin")} style={{cursor: 'pointer'}}>
+                            <InstagramIcon />
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
     )
