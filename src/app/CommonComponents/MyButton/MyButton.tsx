3f9d7e50551d505b81b0cb9810e8f1b125bce016
Diogo Manuel Brito Pires <30441515+El-Banderas@users.noreply.github.com>
2024-11-17 18:58:32 +0000

    change btn

--- src/app/CommonComponents/MyButton/MyButton.tsx
+++ src/app/CommonComponents/MyButton/MyButton.tsx
@@ -1,6 +1,7 @@
 
 
 import Button from '@mui/material/Button';
+import styles from "./MyButton.style.module.css";
 
 interface MyButtonProps {
         text: string,
@@ -8,5 +9,5 @@ interface MyButtonProps {
 }
 
 export default function MyButton  (props : MyButtonProps) {
-        return <Button  variant="outlined" onClick={props.onClick}>{props.text}</Button>
+        return <Button className={styles.button}  variant="outlined" onClick={props.onClick}>{props.text}</Button>
 }
