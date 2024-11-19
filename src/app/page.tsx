6ef2babe9fee636236a0b69236cf4b9f0fb9240c
Diogo Manuel Brito Pires <30441515+El-Banderas@users.noreply.github.com>
2024-11-19 22:26:01 +0000

    Meta data done?

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import { Metadata } from 'next';
 import styles from "./page.module.css";
 import AboutUs from "./mainPage/AboutUs";
 import Contacts from "./mainPage/Contacts";
@@ -7,11 +6,6 @@ import Contacts from "./mainPage/Contacts";
 import roverLandPage from '/public/roverLandPage.jpg'
 import ImageWText from "./CommonComponents/ImageWText/ImageWText";
 
-export const metadata: Metadata = {
-  title: 'Taurus Space',
-  description:
-    'Where Strenght meets the stars',
-};
 
 
 export default function Home() {
