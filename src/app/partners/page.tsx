c0d757e8cbf7866f7405ffbcfe974a36f0103c9e
Diogo Manuel Brito Pires <30441515+El-Banderas@users.noreply.github.com>
2025-02-16 16:39:21 +0000

    Init partners

--- src/app/partners/page.tsx
+++ src/app/partners/page.tsx
@@ -1,12 +1,27 @@
+'use client'
 
+import React, { useState} from 'react';
 import styles from './styles.module.css'
 import partners from './partners'
 
 
 export default function Page() {
     
-// Reuse the stuff from about-us page ;)
+  const [option, setOption] = useState<string>("Partners");
+
+  function selectOption(selected : string) {
+    setOption(selected)
+    console.log(selected)
+  }
   return <div className={styles.content}>
-    Partners
+    <h1 className={styles.title}>
+    Our ?Amazing? Supporters
+</h1>
+<div className={styles.toggle}>
+  {["Partners", "Sponsors"].map(thisOption =>  
+  <div className={`${styles.option} ${option === thisOption && styles.selectedOption}` } onClick={() => selectOption(thisOption)}>{thisOption}</div>
+    )
+  }
+</div>
     </div>;
 }
--- src/app/partners/partners.tsx
+++ src/app/partners/partners.tsx
@@ -1,4 +1,6 @@
 
+import partner1 from '/public/partner1.jpg'
+import partner2 from '/public/partner2.png'
 const partners = {
     "partners" : {
         "P1" : {
