15850e613c9e2bd7270769f74a95274a276abe50
Diogo Manuel Brito Pires <30441515+El-Banderas@users.noreply.github.com>
2025-10-01 00:04:39 +0100

    Renders lists

--- src/app/CommonComponents/RowBtns.tsx/RowBtns.tsx
+++ src/app/CommonComponents/RowBtns.tsx/RowBtns.tsx
@@ -11,15 +11,15 @@ interface RowBtnsProps {
 export default function RowBtns({ departmentsTitles, selectedDPT, selectDPT }: RowBtnsProps ) {
 
   const isUnderlined = (text: string) => {
-    if (text === selectedDPT) return 'underline'
-    return 'none'
+    return text === selectedDPT ? styles.underlined : ''
   }
 
   return (
 <ul className={styles.myList}>
-  <div className={styles.blackBox} ><li><a href="#" >Welcome to my website</a></li></div>
-  <div className={styles.blackBox} ><li className={styles.underlined}><a href="#" >Welcome to my website</a></li></div>
-  <div className={styles.blackBox} ><li><a href="#"> Welcome to my website</a></li></div>
+  {departmentsTitles.map(
+    title => 
+  <div className={styles.blackBox} onClick={() => selectDPT(title)} key={title}><li className={isUnderlined(title)}><a href="#" >{title}</a></li></div>
+  )}
 </ul>
   );
 
