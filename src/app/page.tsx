1f75ad55c1a18a3828100bf4e4790a583e821069
Diogo Manuel Brito Pires <30441515+El-Banderas@users.noreply.github.com>
2025-12-12 19:37:28 +0000

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import styles from "./page.module.css";
 import AboutUs from "./mainPage/AboutUs";
 import Contacts from "./mainPage/Contacts";
 
-import mainPage from '/public/MainPage.jpg'
+import mainPage from '/public/MainPage_LQ.jpg'
 import ImageWText from "./CommonComponents/ImageWText/ImageWText";
 
 
